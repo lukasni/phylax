24f68d4ee8d9659fd28ab31426c9d5faca82f456
Lukas Niederberger <niederbergerlukas@gmail.com>
2022-11-19 22:50:04 -0800

    Improved RedisQ Client logic

--- lib/phylax/zkillboard/redisq_client.ex
+++ lib/phylax/zkillboard/redisq_client.ex
@@ -7,13 +7,18 @@ defmodule Phylax.Zkillboard.RedisqClient do
 
   @name __MODULE__
   @killmail_url "https://zkillboard.com/kill"
+  @initial_state %{
+    tries: 0,
+    last_result: nil,
+    processed: 0
+  }
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, [], name: @name)
   end
 
   def get_state() do
-    GenServer.call(@name, :get_state)
+    GenServer.call(@name, :get_state, :timer.seconds(config()[:ttw]))
   end
 
   ##################
@@ -21,8 +26,8 @@ defmodule Phylax.Zkillboard.RedisqClient do
   ##################
 
   def init(_) do
-    send(self(), :next)
-    {:ok, 0}
+    Process.send_after(self(), :next, :timer.seconds(config()[:ttw]))
+    {:ok, @initial_state}
   end
 
   def handle_call(:get_state, _from, state) do
@@ -32,14 +37,27 @@ defmodule Phylax.Zkillboard.RedisqClient do
   def handle_info(:next, state) do
     state =
       case process_response(fetch_next()) do
-        %Kill{} ->
-          state + 1
+        {:ok, %Kill{} = result} ->
+          Phylax.broadcast(result)
 
-        _ ->
           state
+          |> Map.update!(:processed, &(&1 + 1))
+          |> Map.put(:tries, 0)
+          |> Map.put(:last_result, result)
+
+        {:ok, :empty_package} ->
+          state
+
+        {:error, {:rate_limiting, _} = result} ->
+          state
+          |> Map.update!(:tries, &(&1 + 1))
+          |> Map.put(:last_result, result)
+
+        {:error, result} ->
+          %{state | last_result: result}
       end
 
-    send(self(), :next)
+    Process.send_after(self(), :next, backoff(state.tries))
     {:noreply, state}
   end
 
@@ -51,34 +69,25 @@ defmodule Phylax.Zkillboard.RedisqClient do
   # Helper Functions
   ##################
 
+  @doc """
+  Processes the RedisQ response, returns a 3-tuple of the result type, result and the delay to next fetch.
+  """
   def process_response(response) do
     with {:ok, %{body: body, status: 200}} <- response,
          {:ok, %{"package" => package}} <- Jason.decode(body),
          {:ok, killmail} <- extract_package(package),
          {:ok, kill} <- parse_killmail(killmail) do
       Logger.debug("Received Kill #{kill.kill_id}")
-      Phylax.broadcast(kill)
-      kill
+      {:ok, kill}
     else
       {:ok, %{status: 429} = response} ->
-        Logger.info("Rate Limiting: #{inspect(response)}")
-        Logger.info("Sleeping for 10 seconds")
-        :timer.sleep(:timer.seconds(10))
+        {:error, {:rate_limiting, response}}
 
-      {:error, %Finch.Error{} = error} ->
-        Logger.info("HTTP Error: #{inspect(error)}")
-
-      {:error, %Jason.DecodeError{} = error} ->
-        Logger.info("Decode Error: #{inspect(error)}\n\nResponse: #{inspect(response)}")
-
-      {:error, :empty_package} ->
-        Logger.debug("Empty package. No killmails in past 10 seconds")
-
-      {:error, {:invalid_killmail, killmail, error}} ->
-        Logger.info("Invalid Killmail: #{inspect(killmail)}. Raised error: #{inspect(error)}")
+      :empty_package ->
+        {:ok, :empty_package}
 
       error ->
-        Logger.info("Unexpected error handling killmail: #{inspect(error)}")
+        error
     end
   end
 
@@ -87,7 +96,7 @@ defmodule Phylax.Zkillboard.RedisqClient do
     |> Finch.request(FinchClient)
   end
 
-  def extract_package(nil), do: {:error, :empty_package}
+  def extract_package(nil), do: :empty_package
 
   def extract_package(%{"killID" => kill_id, "killmail" => killmail, "zkb" => zkb}) do
     # This should probably not be done here, but I really want to keep the clients compatible.
@@ -122,6 +131,12 @@ defmodule Phylax.Zkillboard.RedisqClient do
     [
       {"User-Agent", config()[:user_agent]}
     ]
+  end
+
+  def backoff(tries) do
+    (config()[:ttw] * tries)
+    |> min(60)
+    |> :timer.seconds()
   end
 
   def config do
